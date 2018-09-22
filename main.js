8c77addceb08ed5c8ac6f29958c30568fe15de19
Lance Hamilton <lance.hamilton24@gmail.com>
2018-09-22 13:48:00 -0500

    created function to convert letters into number value

--- main.js
+++ main.js
@@ -1,4 +1,28 @@
-// const printToDom = (stringToPrint, elementId) => {
-//     const selectedDiv = document.getElementById(elementId);
-//     selectedDiv.innerHTML = stringToPrint;
-// };
+const codeConversion = ['h', 'e', 'l', 'l', 'o'];
+// var index = '';
+
+
+const printToDom = (stringToPrint, elementId) => {
+    const selectedDiv = document.getElementById(elementId);
+    selectedDiv.innerHTML = stringToPrint;
+};
+
+const codeMaker = () => {
+    const inputValue = document.getElementById('secretInputBox').value
+    const inputWords = inputValue
+    const numberConversionString = numberStringBuilder(inputWords);
+    printToDom(numberConversionString.charCodeAt(), 'returnNumbers');
+}
+
+const numberStringBuilder = (inputWords) => {
+    let newString = '';
+    for(let i=0; i<inputWords.length;i++) {
+        let convertedNumber = codeConversion[inputWords[i]];
+        newString += `<p>${convertedNumber}</p>`
+
+    };
+    return newString
+}
+
+const numberButton = document.getElementById('secretButton');
+    numberButton.addEventListener("click", codeMaker);
